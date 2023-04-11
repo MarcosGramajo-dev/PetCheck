1deaf72ee26a85e810fdc645e78764c81fa78be7
Marcos <marcosgramajo96@gmail.com>
2023-04-10 21:08:00 -0300

    background

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,15 @@ import ConteinerCard  from './components/conteinerCard'
 function App() {
 
   return (
-    <div className="relative max-w-[900px] m-auto shadow-xl min-h-full pb-5 overflow-hidden backGroundImage">
-      <Nav/>
-      <Info/>
-      <Search/>
-      <ConteinerCard/>
+    <div>
+      <div className='absolute w-full h-full background-complete'>
+      </div>
+      <div className="bg-white z-10 relative max-w-[900px] m-auto shadow-xl min-h-full pb-5 overflow-hidden backGroundImage">
+        <Nav/>
+        <Info/>
+        <Search/>
+        <ConteinerCard/>
+      </div>
     </div>
   )
 }
