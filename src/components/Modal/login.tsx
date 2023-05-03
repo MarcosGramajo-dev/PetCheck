0c453ddf8ce85719be6d865bccbec5aa95a0c664
lourdeesmurua <lourbelenmuruavez@gmail.com>
2023-05-02 23:11:48 -0300

    se agrego validacion de cantidad de caracteres

--- src/components/Modal/login.tsx
+++ src/components/Modal/login.tsx
@@ -1,3 +1,4 @@
+import React, { useContext, useState } from 'react';
 import Close from '../../images/icons/close.svg'
 import { Link } from 'react-router-dom'
 
@@ -7,19 +8,53 @@ interface IProps {
 }
 
 export default function Login(props: IProps) {
+   
+  const[username, setUsername]= useState("");
+  const[password, setPassword]= useState("");
+  const [errorMessage, setErrorMessage]= React.useState<string | null>();
+  console.log(username);
+  console.log(password)
+
+  const hadleUsernameChange= (event: React.ChangeEvent<HTMLInputElement>)=>{
+    setUsername(event.target.value);
+    if(username.length<4){
+      setErrorMessage("El nombre es demasiado corto");
+      // return;
+    }
+  } 
+
+  const handlePasswordChange=(event: React.ChangeEvent<HTMLInputElement>)=>{
+    setPassword(event.target.value);
+    if(password.length<6){
+      setErrorMessage("La contraseña debe tener al menos 6 caracteres");
+      // return;
+    }else{
+      setErrorMessage("")
+    }
+  }
+
+  const handleSubmit=(event:React.FormEvent<HTMLFormElement>)=>{
+    event.preventDefault();
+    setErrorMessage("");
+    console.log(username, username.length)
+    
+    props.toggleOpen();
+    props.toggleLogin()
+  }
   return (
     <div className="relative">
-      <form className="flex flex-col justify-center items-center py-3">
+      <form className="flex flex-col justify-center items-center py-3" onSubmit={handleSubmit}>
         <p className=" text-xl pb-2">Iniciar Sesion</p>
         <div className="relative w-60 object-cover my-2">
-          <input className="my-1 w-60 px-2 border-b-4 border-t-2 border-vet-purple-light rounded-md" type="text" placeholder='Usuario' />
+          <input className="my-1 w-60 px-2 border-b-4 border-t-2 border-vet-purple-light rounded-md" type="text" placeholder='Usuario'  onChange={hadleUsernameChange} />
         </div>
         <div className="relative w-60 object-cover my-2">
-          <input className="my-1 w-60 px-2 border-b-4 border-t-2 border-vet-purple-light rounded-md" type="password" placeholder='Password' />
+          <input className="my-1 w-60 px-2 border-b-4 border-t-2 border-vet-purple-light rounded-md" type="password" placeholder='Password'  onChange={handlePasswordChange}/>
         </div>
+        {errorMessage ? <p className='text-red-500'>{errorMessage}</p>:"" }
         <button onClick={(event)=> {event.preventDefault(); props.toggleOpen(); props.toggleLogin()}} className="my-2 w-32 px-4 h-8 border bg-white border-vet-purple text-vet-purple rounded-lg hover:text-white hover:bg-vet-purple hover:border-white text-sm">Iniciar Sesion</button>
         <Link to="register" className="text-vet-purple-dark text-xs my-2"> ¿No tiene Cuenta?. Registrate Aquí</Link>
-      </form>
+      </form >
     </div>
   )
 }
