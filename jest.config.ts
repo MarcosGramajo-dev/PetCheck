085726069dcff45faccfd22b832fadf979fba84e
Marcos <marcosgramajo96@gmail.com>
2023-06-28 13:40:10 -0300

    esto no va

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from '@jest/types'
+
+const config : Config.InitialOptions={
+  preset:'ts-jest',
+  testEnvironment:'jsdom',
+  verbose: true
+}
+
+
+
+export default config;
