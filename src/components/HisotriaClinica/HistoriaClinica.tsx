90e34c9b8f0aa4f084e14152938e0d119c98208f
Marcos <marcosgramajo96@gmail.com>
2023-06-28 19:06:43 -0300

    stilos en HC

--- src/components/HisotriaClinica/HistoriaClinica.tsx
+++ src/components/HisotriaClinica/HistoriaClinica.tsx
@@ -74,48 +74,59 @@ const HistoriaClinica: React.FC = () => {
     <div>
       <div>
         <div>
-          <p>ID: {HC?.id}</p>
-          <p>Dueño de la Mascota</p>
-          <p>{HC?.ownerPet.NombreDueño}</p>
-          <p>{HC?.ownerPet.DNI}</p>
-          <p>{HC?.ownerPet.Direccion}</p>
-          <p>{HC?.ownerPet.Telefono}</p>
-          <p>{HC?.ownerPet.province}</p>
-          <p>{HC?.ownerPet.departament}</p>
-        </div>
-        <div>
-          <p>Mascota</p>
-          <img src={HC?.DataPet.image} alt="foto mascota"/>
-          <p>{HC?.DataPet.NombreMascota}</p>
-          <p>{HC?.DataPet.Sexo}</p>
-          <p>{HC?.DataPet.Date}</p>
-          <p>{HC?.DataPet.Pedigree}</p>
-          <p>{HC?.DataPet.Nchip}</p>
-          <p>{HC?.DataPet.detalles}</p>
-        </div>
-        <div>
-          <p>Resumen Clinico</p>
-          <p>Vacunacion</p>
-          {HC?.Vacunas.map((element) =>(
-              <div>
-                <p>{element.Vacuna}</p>
-                <p>{element.DataVacuna}</p>
-                <p>{element.fecha}</p>
-                <p>{element.Certification}</p>
-                <p>{element.nameAndMatricule}</p>
-              </div>
-          ))}
+          <div className="max-sm:w-full max-sm:border-0 w-11/12 p-5 m-auto my-10 bg-white/80 border-8 border-vet-purple-light rounded-lg max-w-[900px] flex justify-around items-center">
+            <div>
+              <img className="w-[200px]" src={HC?.DataPet.image} alt="foto mascota"/>
+            </div>
+            <div>
+              <p className="font-semibold text-vet-blue text-2xl text-left">PERFIL DE LA MASCOTA</p>
+              <p className="m-1 font-semibold">Nombre: <span className="font-normal">{HC?.DataPet.NombreMascota}</span> </p>
+              <p className="m-1 font-semibold">Sexo: <span className="font-normal">{HC?.DataPet.Sexo}</span></p>
+              <p className="m-1 font-semibold">Fecha de Nac: <span className="font-normal">{HC?.DataPet.Date}</span></p>
+              <p className="m-1 font-semibold">Pedigree: <span className="font-normal">{HC?.DataPet.Pedigree}</span></p>
+              <p className="m-1 font-semibold">Nchip: <span className="font-normal">{HC?.DataPet.Nchip}</span></p>
+              <p className="m-1 font-semibold">Detalle: <span className="font-normal">{HC?.DataPet.detalles}</span></p>
+            </div>
+          </div>
+          <div className="max-sm:w-full max-sm:border-0 w-11/12 p-5 m-auto my-10 bg-white/80 border-8 border-vet-purple-light rounded-lg max-w-[900px] flex flex-col justify-center">
+            <div>
+              <p className="font-semibold text-vet-blue text-2xl text-left">PERFIL DEL DUEÑO</p>
+              <p className="m-1 font-semibold">Nombre: <span className="font-normal">{HC?.ownerPet.NombreDueño}</span> </p>
+              <p className="m-1 font-semibold">DNI: <span className="font-normal">{HC?.ownerPet.DNI}</span></p>
+              <p className="m-1 font-semibold">Direccion: <span className="font-normal">{HC?.ownerPet.Direccion}</span></p>
+              <p className="m-1 font-semibold">Telefono: <span className="font-normal">{HC?.ownerPet.Telefono}</span></p>
+              <p className="m-1 font-semibold">Provincia: <span className="font-normal">{HC?.ownerPet.province}</span></p>
+              <p className="m-1 font-semibold">Localidad: <span className="font-normal">{HC?.ownerPet.departament}</span></p>
+            </div>
+          </div>
+          <div className="max-sm:w-full max-sm:border-0 w-11/12 p-5 m-auto my-10 bg-white/80 border-8 border-vet-purple-light rounded-lg max-w-[900px] flex justify-around items-center">
+            <div>
+              <p className="font-semibold text-vet-blue text-2xl text-left">RESUMEN CLINICO</p>
+              <p className="m-1 font-semibold text-vet-blue">Vacunacion</p>
+              {HC?.Vacunas.map((element) =>(
+                <div>
+                  <p className="m-1 font-semibold"><span className="font-normal">{element.Vacuna}</span></p>
+                  <p className="m-1 font-semibold"><span className="font-normal">{element.DataVacuna}</span></p>
+                  <p className="m-1 font-semibold"><span className="font-normal">{element.fecha}</span></p>
+                  <p className="m-1 font-semibold"><span className="font-normal">{element.Certification}</span></p>
+                  <p className="m-1 font-semibold"><span className="font-normal">{element.nameAndMatricule}</span></p>
+                </div>
+              ))}
+            </div>
+            <div>
+            <p className="m-1 font-semibold text-vet-blue">Historia Clinica</p>
+              {HC?.Registros.map((element) =>(
+                <div>
+                  <p className="m-1 font-semibold"><span className="font-normal">{element.Registro}</span></p>
+                  <p className="m-1 font-semibold"><span className="font-normal">{element.fecha}</span></p>
+                  <p className="m-1 font-semibold"><span className="font-normal">{element.Info}</span></p>
+                </div>
+              ))}
 
-          {HC?.Registros.map((element) =>(
-              <div>
-                <p>{element.Registro}</p>
-                <p>{element.fecha}</p>
-                <p>{element.Info}</p>
-              </div>
-          ))}
+            </div>
+          </div>
         </div>
       </div>
-      <button onClick={() => handleChange()}> Presiona </button>
     </div>
   );
 };
