de1816de6313a7dd69d03d86e87fec8e4047fe30
Bertz96 <99715943+Bertz96@users.noreply.github.com>
2023-06-29 01:50:13 -0300

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  presets: [
-    ["@babel/preset-env", { targets: { node: "current" } }],
-    "@babel/preset-typescript",
-  ],
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
 };
